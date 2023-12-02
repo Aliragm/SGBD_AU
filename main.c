ae8640f1738a66b5a0f135c26a6e509c4aa84371
André Lira <140771656+Aliragm@users.noreply.github.com>
2023-12-02 16:10:30 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,41 +8,55 @@ void criar_tabela(){
     char tipo_dado, nome_tabela[21];
     int linhas, colunas, i, j;
     printf("Digite a quantidade de linhas e colunas respectivamente:\n");
-    scanf("%i %i", &linhas, &colunas);
-    printf("Selecione o tipo de dado:\n");
+    printf("Linhas: ");
+    scanf("%i", &linhas);
+    printf("\nColunas: ");
+    scanf("%i", &colunas);
+    printf("\nSelecione o tipo de dado:\nInt = i, Char = c, Float = f, Double = d\n:");
     getchar();
     scanf("%c", &tipo_dado);
-    printf("Qual sera o nome da tabela? (Máx. 20 char)\n");
+    printf("Qual sera o nome da tabela? (Max. 20 char)\n");
     getchar();
     fgets(nome_tabela, 20, stdin);
+    nome_tabela[strcspn(nome_tabela, "\n")] = 0;
+    strcat(nome_tabela, ".txt");
 
     if(tipo_dado == 'i'){
         int tabela[linhas][colunas];
         for (i = 0; i < linhas; i++){
-            printf("Digite a %i° linha\n", i + 1);
+            printf("Digite a linha %i\n", i + 1);
             for (j = 0; j < colunas; j++){
+                printf("Coluna %i: ", j + 1);
                 scanf("%i", &tabela[i][j]);
             }
         }
         arquivo_tabela = fopen(nome_tabela,"w");
+        if(arquivo_tabela == NULL) {
+            perror("Erro ao abrir o arquivo");
+            return;
+        }
         for (i = 0; i < linhas; i++){
-            printf("\n");
             for (j = 0; j < colunas; j++){
                 fprintf(arquivo_tabela, "%i ", tabela[i][j]);
             }
+            
         }
     }
     else if(tipo_dado == 'c'){
         char tabela[linhas][colunas];
         for (i = 0; i < linhas; i++){
-            printf("Digite a %i° linha\n", i + 1);
+            printf("Digite a linha %i\n", i + 1);
             for (j = 0; j < colunas; j++){
+                printf("Coluna %i: ", j + 1);
                 scanf("%c", &tabela[i][j]);
             }
         }
         arquivo_tabela = fopen(nome_tabela,"w");
+        if(arquivo_tabela == NULL) {
+            perror("Erro ao abrir o arquivo");
+            return;
+        }
         for (i = 0; i < linhas; i++){
-            printf("\n");
             for (j = 0; j < colunas; j++){
                 fprintf(arquivo_tabela, "%c ", tabela[i][j]);
             }
@@ -51,13 +65,18 @@ void criar_tabela(){
     else if(tipo_dado == 'f'){
         float tabela[linhas][colunas];
         for (i = 0; i < linhas; i++){
-            printf("Digite a %i° linha\n", i + 1);
+            printf("Digite a linha %i\n", i + 1);
             for (j = 0; j < colunas; j++){
+                printf("Coluna %i: ", j + 1);
                 scanf("%f", &tabela[i][j]);
             }
         }
+        arquivo_tabela = fopen(nome_tabela,"w");
+        if(arquivo_tabela == NULL) {
+            perror("Erro ao abrir o arquivo");
+            return;
+        }
         for (i = 0; i < linhas; i++){
-            printf("\n");
             for (j = 0; j < colunas; j++){
                 fprintf(arquivo_tabela, "%f ", tabela[i][j]);
             }
@@ -66,44 +85,24 @@ void criar_tabela(){
     else if(tipo_dado == 'd'){
         double tabela[linhas][colunas];
         for (i = 0; i < linhas; i++){
-            printf("Digite a %i° linha\n", i + 1);
+            printf("Digite a linha %i\n", i + 1);
             for (j = 0; j < colunas; j++){
+                printf("Coluna %i: ", j + 1);
                 scanf("%lf", &tabela[i][j]);
             }
         }
+        arquivo_tabela = fopen(nome_tabela,"w");
+        if(arquivo_tabela == NULL) {
+            perror("Erro ao abrir o arquivo");
+            return;
+        }
         for (i = 0; i < linhas; i++){
-            printf("\n");
             for (j = 0; j < colunas; j++){
                 fprintf(arquivo_tabela, "%lf ", tabela[i][j]);
             }
         }
-    }
-/*    
-    else if(tipo_dado == 's'){
-        char *tabela[linhas][colunas];
-        for (i = 0; i < linhas; i++){
-            printf("Digite a %i° linha\n", i + 1);
-            for (j = 0; j < colunas; j++){
-                
-                scanf("%s", tabela[i][j]);
-            }
-        }
-        for (i = 0; i < linhas; i++){
-            printf("\n");
-            for (j = 0; j < colunas; j++){
-                fprintf(arquivo_tabela, "%s ", tabela[i][j]);
-            }
-        }
-    }
-*/
-
-    
-    
-    
+    }//implementar strings
 }
-
-
-
 
 int main(){
     criar_tabela();
